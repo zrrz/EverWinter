a83632b44f915c587f52235f96d9274121301541
Eric Espinoza <espi_71@hotmail.com>
2014-01-07 04:00:03 -0800

    Added test scene to test BulletManager. Looking good so far.

---
+++ Assets/testshoot.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class testshoot : MonoBehaviour {
+
+	public Transform shootPoint;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetMouseButtonDown(0)) {
+			BulletManager.instance.Shoot(true, BulletType.normal, shootPoint.position, shootPoint.transform.forward);
+		}
+	}
+}
